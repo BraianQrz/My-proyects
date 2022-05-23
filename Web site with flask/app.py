9086fa29018d22590819e2e454d040cd428ea5a8
aplanadora38 <bquiroz801@gmail.com>
2022-05-23 11:30:36 -0300

    add database interface with flask

--- Web site with flask/app.py
+++ Web site with flask/app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template, request, redirect ,url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# Mysql Local connection
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'contact_users'
 mysql = MySQL(app)
 
+
+# session
+app.secret_key = 'mysecretkey'
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM contacts')
+    alldata = cursor.fetchall()
+    print(alldata)
+    return render_template('index.html', contact_data=alldata)
+
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
@@ -19,8 +31,10 @@ def add_contact():
         phone = request.form['phone']
         email = request.form['email']
         cursor = mysql.connection.cursor()
-        cursor.execute('INSERT INTO contacts (nombre,telefono,email) VALUES(%s,%s,%s)',(fullname, phone, email))
+        cursor.execute(
+            'INSERT INTO contacts (nombre,telefono,email) VALUES(%s,%s,%s)', (fullname, phone, email))
         mysql.connection.commit()
+        flash('Contacto agregado satisfactoriamente')
     return redirect(url_for('index'))
 
 
@@ -28,12 +42,16 @@ def add_contact():
 def edit_cotact():
     return 'edit_contact'
 
-@app.route('/delete_contact')
-def delete_contact():
-    return 'Delete_contact'
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute(f'DELETE FROM contacts WHERE id = {id}')
+    mysql.connection.commit()
+    flash('Contacto removido satisfactotriamente')
+    return redirect(url_for('index'))
+
 
 
 if __name__ == "__main__":
-    app.run(port = 3000, debug = True)
-
-
+    app.run(port=3000, debug=True)
